ba4e2de84132db2bee20231242d6d1ffbf97dd46
neb <nebb_bb@protonmail.com>
2022-01-24 14:33:07 -0500

    new db setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,6 @@ import {
 } from "rebass/styled-components";
 import { UserContext } from "pages/_app";
 
-import router, { useRouter } from "next/router";
-
 export default function Header() {
   const { user } = React.useContext(UserContext);
   const STREAM_LABS_AUTH = `https://streamlabs.com/api/v1.0/authorize?client_id=${process.env.NEXT_PUBLIC_STREAMLABS_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_STREAMLABS_REDIRECT_URI}&scope=donations.create+alerts.create&response_type=code`;
