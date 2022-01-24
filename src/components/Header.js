ee6b025e3ac53e3c052c9bd96157a51153b8b9d9
neb <nebb_bb@protonmail.com>
2022-01-24 14:38:53 -0500

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,12 +9,15 @@ import {
 } from "rebass/styled-components";
 import { UserContext } from "pages/_app";
 
+import router, { useRouter } from "next/router";
+
 export default function Header() {
+  const router = useRouter();
   const { user } = React.useContext(UserContext);
   const STREAM_LABS_AUTH = `https://streamlabs.com/api/v1.0/authorize?client_id=${process.env.NEXT_PUBLIC_STREAMLABS_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_STREAMLABS_REDIRECT_URI}&scope=donations.create+alerts.create&response_type=code`;
 
   return (
-    <Flex alignItems="flex-end" justifyContent="space-between" py={4} mb={[4]}>
+    <Flex alignItems="flex-end" justifyContent="space-between" py={4}>
       <Link href="/" style={{ textDecoration: "none" }}>
         <Text
           fontWeight="bold"
@@ -31,9 +34,16 @@ export default function Header() {
       </Link>
       {!user.isLoggedIn && (
         <Link href={STREAM_LABS_AUTH}>
-          <Button variant="secondary" mt={2}>
-            <Text display={["none", "block"]}>Login with Streamlabs</Text>
-            <Text display={["block", "none"]}>Login</Text>
+          <Button
+            variant="secondary"
+            mt={2}
+            onClick={() =>
+              router.push({
+                pathname: router.pathname,
+              })
+            }
+          >
+            <Text display={["none", "block"]}></Text>Login with Streamlabs
           </Button>
         </Link>
       )}
