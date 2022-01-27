ccad8fec7da1b646853c8dd3f7336db225bfb092
neb <nebb_bb@protonmail.com>
2022-01-27 17:08:49 -0500

    fix typo

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,7 +6,7 @@ const Head = (props) => {
       <title>
         {process.env.NODE_ENV === "development" ? "DEV - " : ""}dono.at
       </title>
-      <meta property="og:image" content="https://dono.at/og.png" />
+      <meta property="og:image" content="https://dono.at/assets/og.png" />
 
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </NextHead>
