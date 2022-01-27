8612e232d203af628e0377bb5941e02239f3753d
neb <nebb_bb@protonmail.com>
2022-01-27 01:15:51 -0500

    fix typo

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,7 +3,9 @@ import NextHead from "next/head";
 const Head = (props) => {
   return (
     <NextHead>
-      <title>{process.env.NODE_ENV === "development" && "DEV - "}dono.at</title>
+      <title>
+        {process.env.NODE_ENV === "development" ? "DEV - " : ""}dono.at
+      </title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </NextHead>
   );
