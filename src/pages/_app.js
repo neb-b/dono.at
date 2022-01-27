72f7ca4fb4ffa8bf34381e27b05f51b8ef4cb2c4
neb <nebb_bb@protonmail.com>
2022-01-26 23:35:05 -0500

    test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,17 +12,22 @@ export const UserContext = React.createContext();
 
 function MyApp({ Component, pageProps }) {
   const [user, setUser] = React.useState({});
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => setMounted(true), []);
 
   return (
     <UserContext.Provider value={{ setUser, user }}>
       <ThemeProvider theme={theme}>
-        <Flex flexDirection="column" minHeight="100vh">
-          <Box px={[4]}>
-            <Header />
-            <Component {...pageProps} />
-          </Box>
-          <Footer />
-        </Flex>
+        <Box sx={{ visibility: mounted ? "" : "hidden" }}>
+          <Flex flexDirection="column" minHeight="100vh">
+            <Box px={[4]}>
+              <Header />
+              <Component {...pageProps} />
+            </Box>
+            <Footer />
+          </Flex>
+        </Box>
       </ThemeProvider>
     </UserContext.Provider>
   );
