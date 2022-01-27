1f48652b14b8f683c0c0da25b5fbe5a11f0c68d0
neb <nebb_bb@protonmail.com>
2022-01-27 17:10:58 -0500

    add description

--- src/components/Head.js
+++ src/components/Head.js
@@ -7,6 +7,10 @@ const Head = (props) => {
         {process.env.NODE_ENV === "development" ? "DEV - " : ""}dono.at
       </title>
       <meta property="og:image" content="https://dono.at/assets/og.png" />
+      <meta
+        property="og:description"
+        content="Receive Bitcoin donations with Streamlabs alerts"
+      />
 
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </NextHead>
