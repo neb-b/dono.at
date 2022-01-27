0638c6228005e77f6d3817f6a2decde30bc63e3d
neb <nebb_bb@protonmail.com>
2022-01-27 01:14:16 -0500

    remove dev from title in prod

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,7 +3,7 @@ import NextHead from "next/head";
 const Head = (props) => {
   return (
     <NextHead>
-      <title>DEV - dono.at</title>
+      <title>{process.env.NODE_ENV === "development" && "DEV - "}dono.at</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </NextHead>
   );
